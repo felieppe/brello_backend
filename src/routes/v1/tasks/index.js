fcaf28bbf8879d1bff84df7b5bcc3329c53523d8
Felipe Cabrera <me@felieppe.com>
2024-09-02 12:54:28 -0300

    fix: replace assigned field value from String to an Array containing Int

--- src/routes/v1/tasks/index.js
+++ src/routes/v1/tasks/index.js
@@ -6,7 +6,7 @@ const tasks = [
         id: 1,
         title: "Task 1",
         description: "This is the first task.",
-        assigned: "John Doe",
+        assigned: [1],
         priority: 3,
         state: 3,
         limit: undefined
@@ -20,7 +20,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     const id = parseInt(req.params.id)
     const task = tasks.find(task => task.id === id)
-    
+
     if (!task) return res.status(404).json({ success: false, data: null, message: "Task not found." })
     return res.status(200).json({ success: true, data: task, message: "Found task." })
 })
