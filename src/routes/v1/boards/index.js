f1dca319c6a914b1ad7a97c75cf1182a45e0a679
Felipe Cabrera <me@felieppe.com>
2024-09-04 11:24:05 -0300

    feat: add /boards :GET,POST,PUT,PATCH,DELETE routing

---
+++ src/routes/v1/boards/index.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const router = express.Router()
+
+const DB_BOARDS = './db/boards.json'
+var boards = []
+
+function getBoards() {
+    if (!fs.existsSync(DB_BOARDS)) {
+        fs.mkdirSync(DB_BOARDS.split('/').slice(0, -1).join('/'), { recursive: true })
+        fs.writeFileSync(DB_BOARDS, JSON.stringify([]))
+    }
+
+    return JSON.parse(fs.readFileSync(DB_BOARDS))
+}
+
+function saveBoards() {
+    fs.writeFileSync(DB_BOARDS, JSON.stringify(boards))
+}
+
+router.use((req, res, next) => {
+    boards = getBoards()
+    next()
+})
+
+router.get('/', (req, res) => {
+    return res.status(200).json({ success: true, data: boards, message: "Found boards"})
+})
+
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const board = boards.find(board => board.id === id)
+
+    if (!board) return res.status(404).json({ success: false, data: null, message: "Board not found." })
+    return res.status(200).json({ success: true, data: board, message: "Found board." })
+})
+
+router.post('/', (req, res) => {
+    const { name } = req.body
+    if (!name) return res.status(400).json({ success: false, data: null, message: "Missing fields." })
+
+    const board = { id: uuidv4(), name: name }
+    boards.push(board)
+
+    saveBoards()
+    return res.status(201).json({ success: true, data: board, message: "Board created." })
+})
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id
+    const { name } = req.body
+    if (!name) return res.status(400).json({ success: false, data: null, message: "Missing fields." })
+
+    const board = boards.find(board => board.id === id)
+
+    if (!board) return res.status(404).json({ success: false, data: null, message: "Board not found." })
+
+    board.name = name
+
+    saveBoards()
+    return res.status(200).json({ success: true, data: board, message: "Board updated." })
+})
+
+router.patch('/:id', (req, res) => {
+    const id = req.params.id
+    const { name } = req.body
+
+    const board = boards.find(board => board.id === id)
+
+    if (!board) return res.status(404).json({ success: false, data: null, message: "Board not found." })
+
+    board.name = name
+
+    saveBoards()
+    return res.status(200).json({ success: true, data: board, message: "Board updated." })
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    const board = boards.find(board => board.id === id)
+
+    if (!board) return res.status(404).json({ success: false, data: null, message: "Board not found." })
+
+    boards = boards.filter(board => board.id !== id)
+
+    saveBoards()
+    return res.status(200).json({ success: true, data: board, message: "Board deleted." })
+})
+
+module.exports = router
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const tasks = require('./tasks/')
 const members = require('./members/')
 const columns = require('./columns/')
+const boards = require('./boards/')
 
 router.use('/tasks/', tasks)
 router.use('/members/', members)
 router.use('/columns/', columns)
+router.use('/boards/', boards)
 
 module.exports = router
